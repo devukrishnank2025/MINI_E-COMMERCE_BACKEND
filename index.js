4e2b42b833d8fe9e57f7f0d5213df47a0a4a1a46
devukrishnank2025 <devukrishnank2025@gmail.com>
2026-05-28 14:47:33 +0530

    added main route

--- index.js
+++ index.js
@@ -2,28 +2,36 @@ const express = require("express");
 const connect = require("./DB/db_connection");
 require("dotenv").config()
 
-const cors =require('cors');
+const cors = require("cors");
 
 const adminRoute = require("./Routes/AdminRoutes");
 const productRoute = require("./Routes/ProductRoutes");
-
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 connect();
-app.use(cors());  
-app.use("/ProductImages", express.static("public/productImages"))
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/ProductImages", express.static("public/productImages"));
 
 
-app.use("/addProduct",adminRoute)
-app.use('/readProduct',productRoute)
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Mini E-Commerce Backend Running"
+    });
+});
+
+
+
+app.use("/addProduct", adminRoute);
+app.use("/readProduct", productRoute);
 
 
 app.listen(port, () => {
-    console.log("Server Started");
-})
-
-
+    console.log(`Server Started on Port ${port}`);
+});
